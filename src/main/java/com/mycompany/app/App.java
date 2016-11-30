9eb1330ce13b8e8f4e0bc6218e8a18c87b1763c3
hotspot523 <hspot523@gmail.com>
2016-11-30 15:59:59 +0530

    first commit java test

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,32 @@
+package com.mycompany.app;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        BufferedReader br = null;
+	
+	try {
+
+		String sCurrentLine;
+		
+		br = new BufferedReader(new FileReader(System.getProperty("user.dir")+"/test.txt"));
+
+		while ((sCurrentLine = br.readLine()) != null) {
+			System.out.println(sCurrentLine);
+		}
+
+	} catch (IOException e) {
+		e.printStackTrace();
+	} finally {
+		try {
+			if (br != null)br.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+    }
+}
